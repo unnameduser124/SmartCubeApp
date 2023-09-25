a097a9a19e776bf27aa8fa751ef0894e279f1b4b
ozarskiw <ozarskiw@gmail.com>
2023-09-25 12:56:20 +0200

    PLL match case tests

--- app/src/main/java/com/example/smartcubeapp/casedetection/plldetection/PLLCaseDetection.kt
+++ app/src/main/java/com/example/smartcubeapp/casedetection/plldetection/PLLCaseDetection.kt
@@ -5,7 +5,7 @@ import com.example.smartcubeapp.casedetection.plldetection.pllcase.PredefinedPLL
 import com.example.smartcubeapp.cube.CubeSide
 import com.example.smartcubeapp.cube.CubeState
 
-class PLLCaseDetection(val cubeState: CubeState, private val cubeSide: CubeSide) {
+class PLLCaseDetection(private var cubeState: CubeState, private var cubeSide: CubeSide) {
 
     fun detectPLLCase(): PredefinedPLLCase? {
         TODO("Not implemented yet")
@@ -13,6 +13,14 @@ class PLLCaseDetection(val cubeState: CubeState, private val cubeSide: CubeSide)
 
     fun matchCase(): PredefinedPLLCase? {
         TODO("Not implemented yet")
+    }
+
+    fun changeCubeSide(cubeSide: CubeSide) {
+        this.cubeSide = cubeSide
+    }
+
+    fun changeCubeState(cubeState: CubeState) {
+        this.cubeState = cubeState
     }
 
 }
