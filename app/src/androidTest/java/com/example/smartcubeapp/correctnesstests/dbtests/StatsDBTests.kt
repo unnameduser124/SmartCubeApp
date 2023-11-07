ebbec673571c0be60b8f7c980323326f4fd8cd13
ozarskiw <ozarskiw@gmail.com>
2023-11-07 09:42:14 +0100

    get and update field methods tests for stats database

--- app/src/androidTest/java/com/example/smartcubeapp/correctnesstests/dbtests/StatsDBTests.kt
+++ app/src/androidTest/java/com/example/smartcubeapp/correctnesstests/dbtests/StatsDBTests.kt
@@ -29,8 +29,9 @@ class StatsDBTests {
         dbService.close()
         context.deleteDatabase(StatsDBConstants.TEST_DATABASE_NAME)
     }
+
     @Test
-    fun createStatsTableTest(){
+    fun createStatsTableTest() {
         val cursor = dbService.readableDatabase.rawQuery(
             "SELECT name " +
                     "FROM sqlite_master WHERE type='table' " +
@@ -47,7 +48,7 @@ class StatsDBTests {
     }
 
     @Test
-    fun databasePopulatedWithStats(){
+    fun databasePopulatedWithStats() {
         val cursor = dbService.readableDatabase.rawQuery(
             "SELECT * FROM ${StatsDBConstants.StatsTable.TABLE_NAME}",
             null
@@ -58,46 +59,85 @@ class StatsDBTests {
             val expectedStats = getExpectedStats()
             val statsNames = mutableListOf<String>()
 
-            do{
+            do {
                 statsNames.add(cursor.getString(0))
-            }while(cursor.moveToNext())
+            } while (cursor.moveToNext())
 
             TestCase.assertEquals(expectedStats.size, statsNames.size)
-            for(stat in expectedStats){
+            for (stat in expectedStats) {
                 TestCase.assertTrue(statsNames.contains(stat))
             }
         }
     }
 
-    private fun getExpectedStats(): List<String>{
+    @Test
+    fun getFieldValueTest() {
+        val cursor = dbService.readableDatabase.rawQuery(
+            "SELECT * FROM ${StatsDBConstants.StatsTable.TABLE_NAME}",
+            null
+        )
+
+        cursor.use {
+            TestCase.assertTrue(cursor.moveToFirst())
+            do {
+                val statName =
+                    cursor.getString(cursor.getColumnIndexOrThrow(StatsDBConstants.StatsTable.STATISTIC_NAME_COLUMN))
+                val statValue =
+                    cursor.getString(cursor.getColumnIndexOrThrow(StatsDBConstants.StatsTable.STATISTIC_VALUE_COLUMN))
+                val expectedValue = dbService.getFieldValue(statName)
+                TestCase.assertEquals(statValue.toDouble(), expectedValue)
+            } while (cursor.moveToNext())
+        }
+    }
+
+    @Test
+    fun updateFieldValueTest() {
+        val cursor = dbService.readableDatabase.rawQuery(
+            "SELECT * FROM ${StatsDBConstants.StatsTable.TABLE_NAME}",
+            null
+        )
+
+        cursor.use {
+            TestCase.assertTrue(cursor.moveToFirst())
+            do {
+                val statName =
+                    cursor.getString(cursor.getColumnIndexOrThrow(StatsDBConstants.StatsTable.STATISTIC_NAME_COLUMN))
+                val statValue =
+                    cursor.getString(cursor.getColumnIndexOrThrow(StatsDBConstants.StatsTable.STATISTIC_VALUE_COLUMN))
+                val newValue = statValue.toDouble() + 1
+                dbService.updateFieldValue(statName, newValue.toString())
+                val expectedValue = dbService.getFieldValue(statName)
+                TestCase.assertEquals(newValue, expectedValue)
+            } while (cursor.moveToNext())
+        }
+    }
+
+    private fun getExpectedStats(): List<String> {
         val expectedStats = mutableListOf<String>()
-        for(name in statsNamesList){
-            if(name.contains('X') && !name.contains('Y')){
-                for(numberOfSolves in numberOfSolvesValues){
+        for (name in statsNamesList) {
+            if (name.contains('X') && !name.contains('Y')) {
+                for (numberOfSolves in numberOfSolvesValues) {
                     expectedStats.add(name.replace("X", numberOfSolves.toString()))
                 }
-            }
-            else if(name.contains('X') && name.contains('Y')) {
-                if(name.contains("PLL")){
-                    for(pll in PredefinedPLLCase.values()){
-                        for(numberOfSolves in numberOfSolvesValues){
+            } else if (name.contains('X') && name.contains('Y')) {
+                if (name.contains("PLL")) {
+                    for (pll in PredefinedPLLCase.values()) {
+                        for (numberOfSolves in numberOfSolvesValues) {
                             var modifiedName = name.replace("Y", numberOfSolves.toString())
                             modifiedName = modifiedName.replace("X", pll.name)
                             expectedStats.add(modifiedName)
                         }
                     }
-                }
-                else if(name.contains("OLL")){
-                    for(oll in PredefinedOLLCase.values()){
-                        for(numberOfSolves in numberOfSolvesValues){
+                } else if (name.contains("OLL")) {
+                    for (oll in PredefinedOLLCase.values()) {
+                        for (numberOfSolves in numberOfSolvesValues) {
                             var modifiedName = name.replace("Y", numberOfSolves.toString())
                             modifiedName = modifiedName.replace("OLLX", oll.name)
                             expectedStats.add(modifiedName)
                         }
                     }
                 }
-            }
-            else{
+            } else {
                 expectedStats.add(name)
             }
         }
