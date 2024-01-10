e0e36a535910c610e3dc68b865a3449d94ad1c21
ozarskiw <ozarskiw@gmail.com>
2024-01-10 17:20:38 +0100

    Stats UI refactoring

--- app/src/main/java/com/example/smartcubeapp/ui/statsUI/PhaseStatsSheet.kt
+++ app/src/main/java/com/example/smartcubeapp/ui/statsUI/PhaseStatsSheet.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -19,13 +20,24 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.cube_database.solvedatabase.statsDB.StatsDBConstants
 import com.example.cube_database.solvedatabase.statsDB.StatsService
+import com.example.cube_detection.phasedetection.SolvePhase
 import com.example.smartcubeapp.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
-class PhaseStatsSheet(val phase: com.example.cube_detection.phasedetection.SolvePhase, val context: Context) {
+class PhaseStatsSheet(
+    private val phase: SolvePhase,
+    private val context: Context,
+    private val lifecycleScope: CoroutineScope,
+    private val lifecycle: Lifecycle
+) {
 
     private val statsService = StatsService(context)
+
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun GenerateSheet(onDismiss: () -> Unit) {
@@ -43,15 +55,25 @@ class PhaseStatsSheet(val phase: com.example.cube_detection.phasedetection.Solve
             ) {
                 PhaseNameHeader()
                 TableLabel(labelText = context.getString(R.string.time_averages_table_label))
-                AveragesTable(getTimeAverages()).GenerateTableLayout(context)
+                val timeAverages = remember { mutableStateListOf(Pair("", "")) }
+                val moveAverages = remember { mutableStateListOf(Pair("", "")) }
+                lifecycleScope.launch {
+                    lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                        timeAverages.clear()
+                        moveAverages.clear()
+                        timeAverages.addAll(getTimeAverages())
+                        moveAverages.addAll(getMoveAverages())
+                    }
+                }
+                AveragesTable(timeAverages).GenerateTableLayout(context)
                 TableLabel(labelText = context.getString(R.string.move_averages_table_label))
-                AveragesTable(getMoveAverages()).GenerateTableLayout(context)
+                AveragesTable(moveAverages).GenerateTableLayout(context)
             }
         }
     }
 
     @Composable
-    fun PhaseNameHeader(){
+    fun PhaseNameHeader() {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
@@ -75,12 +97,12 @@ class PhaseStatsSheet(val phase: com.example.cube_detection.phasedetection.Solve
 
     private fun getTimeAverages(): List<Pair<String, String>> {
         val averagesList = mutableListOf<Pair<String, String>>()
+
         val totalSolves = statsService.totalNumberOfSolves()
         StatsDBConstants.numberOfSolvesValues.forEach {
-            if(it > totalSolves) {
+            if (it > totalSolves) {
                 averagesList.add(Pair("-", "-"))
-            }
-            else{
+            } else {
                 val average = com.example.cube_global.millisToSeconds(
                     statsService.averageTimeForPhaseInLastXSolves(
                         it,
@@ -94,7 +116,8 @@ class PhaseStatsSheet(val phase: com.example.cube_detection.phasedetection.Solve
                     )
                 )
                 val averageRounded = com.example.cube_global.roundDouble(average, 100).toString()
-                val bestAverageRounded = com.example.cube_global.roundDouble(bestAverage, 100).toString()
+                val bestAverageRounded =
+                    com.example.cube_global.roundDouble(bestAverage, 100).toString()
                 averagesList.add(Pair(averageRounded, bestAverageRounded))
             }
         }
@@ -105,30 +128,19 @@ class PhaseStatsSheet(val phase: com.example.cube_detection.phasedetection.Solve
         val averagesList = mutableListOf<Pair<String, String>>()
         val totalSolves = statsService.totalNumberOfSolves()
         StatsDBConstants.numberOfSolvesValues.forEach {
-            if(it > totalSolves) {
+            if (it > totalSolves) {
                 averagesList.add(Pair("-", "-"))
-            }
-            else{
+            } else {
                 val average = statsService.averageNumberOfMovesForPhaseInLastXSolves(it, phase)
                 val bestAverage = statsService.bestAverageNumberOfMovesForPhaseInXSolves(it, phase)
                 val averageRounded = com.example.cube_global.roundDouble(average, 10).toString()
-                val bestAverageRounded = com.example.cube_global.roundDouble(bestAverage, 10).toString()
+                val bestAverageRounded =
+                    com.example.cube_global.roundDouble(bestAverage, 10).toString()
                 averagesList.add(Pair(averageRounded, bestAverageRounded))
             }
         }
         return averagesList
     }
-
-    //@OptIn(ExperimentalMaterial3Api::class)
-    //@Composable
-    //fun HideSheetButton(sheetState: SheetState, onDismiss: () -> Unit) {
-    //    val scope = rememberCoroutineScope()
-    //    Button(onClick = {
-    //        scope.launch { sheetState.hide() }.invokeOnCompletion { onDismiss() }
-    //    }) {
-    //        Text(text = "Hide Sheet")
-    //    }
-    //}
 }
 
 @Preview
@@ -136,5 +148,8 @@ class PhaseStatsSheet(val phase: com.example.cube_detection.phasedetection.Solve
 fun PhaseStatsSheetPreview() {
     val sheetOpen = remember { mutableStateOf(true) }
     val context = LocalContext.current
-    PhaseStatsSheet(com.example.cube_detection.phasedetection.SolvePhase.Cross, context).GenerateSheet { sheetOpen.value = false }
+    //PhaseStatsSheet(
+    //    com.example.cube_detection.phasedetection.SolvePhase.Cross,
+    //    context
+    //).GenerateSheet { sheetOpen.value = false }
 }
--- cube-global/src/main/java/com/example/cube_global/GlobalVariables.kt
+++ cube-global/src/main/java/com/example/cube_global/GlobalVariables.kt
@@ -4,5 +4,6 @@ import com.example.cube_cube.cube.Solve
 import com.example.cube_cube.cube.SolveStatus
 
 const val MILLIS_IN_SECOND = 1000
+const val MILLIS_IN_HOUR = 3600000
 var dbAccesses = 0
 var solve = Solve().apply{ solveStatus = SolveStatus.Scramble }
